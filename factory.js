63da6758648e0067eff736e836b6f295fb127b01
Eugene <larychev93@gmail.com>
2019-01-29 23:48:01 +0300

    add factory example

---
+++ factory.js
@@ -0,0 +1,69 @@
+class VehicleFactory {
+   createVehicle (type, options){
+    switch(type) {
+      case 'car':
+        return new Car(options.wheels, options.passengers, options.airbags);
+      case 'bike':
+        return new Bike(options.wheels, options.passengers, options.airbags);
+    }
+  }
+}
+
+class Vehicle {
+  constructor (wheels, airbags, passengers) {
+    this.wheels = wheels;
+    this.airbags = airbags;
+    this.passengers = passengers
+  }
+  get isMoreFun () {
+    return this.wheels === 2
+  }
+
+  get isSaveEnough () {
+    return this.airbags
+  }
+}
+
+class Car extends Vehicle {
+  constructor(wheels = 4, airbags = true, passengers = 3) {
+    super(wheels, airbags, passengers);
+
+    this.bagage = 0
+  }
+
+  mountBagage (weight) {
+    this.bagage = weight
+  }
+}
+
+class Bike extends Vehicle {
+  constructor(wheels = 2, airbags = false, passengers = 1) {
+    super(wheels, airbags, passengers);
+  }
+
+  makeWheelie () {
+    console.log('Hell yeah')
+  }
+}
+
+const vehicleFactory = new VehicleFactory();
+
+const car = vehicleFactory.createVehicle('car', {
+  wheels: 4,
+  passengers: 2,
+  airbags: false
+});
+
+const bike = vehicleFactory.createVehicle('bike', {
+  wheels: 2
+});
+
+console.log(car.isMoreFun);
+console.log(bike.isMoreFun);
+
+car.mountBagage(100);
+bike.makeWheelie();
+
+
+
+
